245d6af168aa9b24e671b6aa5c53a806aad32039
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-05 19:48:05 +0530

    Gig And Genre Models Created

---
+++ MusicGo/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicGo.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
