f078cb4e348ef2b2506fa3c4985807906b95bd2a
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-05 23:14:44 +0530

    UI Improved

--- MusicGo/Controllers/GigsController.cs
+++ MusicGo/Controllers/GigsController.cs
@@ -31,6 +31,7 @@ namespace MusicGo.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             if(!ModelState.IsValid)
