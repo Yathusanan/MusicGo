2259b3718b6507c43b0af3a81c32a3982819a316
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-05 19:01:06 +0530

    Initial Commit

---
+++ MusicGo/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MusicGo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MusicGo.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MusicGo.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
