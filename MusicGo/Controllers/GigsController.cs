5f23d5b59b8e962ed3b8dd384cf011342835f041
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-05 20:37:20 +0530

    Gig Form Created

---
+++ MusicGo/Controllers/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using MusicGo.Models;
+using MusicGo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicGo.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
