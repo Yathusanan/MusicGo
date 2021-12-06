455436fce2be4f1177923503e44011dbd10efb24
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-06 14:12:25 +0530

    Attenace API Added

---
+++ MusicGo/Controllers/AttendancesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using MusicGo.Dtos;
+using MusicGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicGo.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("The attendance Already Exists!");
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- MusicGo/Models/ApplicationDbContext.cs
+++ MusicGo/Models/ApplicationDbContext.cs
@@ -16,10 +16,21 @@ namespace MusicGo.Models
 
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                        .HasRequired(a => a.Gig)
+                        .WithMany()
+                        .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
