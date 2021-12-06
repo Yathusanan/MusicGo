4a8c10164156f3d1dbb25d505254bcbf3404b902
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-06 10:00:44 +0530

    Styles added to Home Page

--- MusicGo/Controllers/HomeController.cs
+++ MusicGo/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace MusicGo.Controllers
         public ActionResult Index()
         {
             var upcomingGigs = _context.Gigs.Include(g => g.Artist)
+                                            .Include(g => g.Genre)
                                             .Where(g => g.DateTime < DateTime.Now);
 
             return View(upcomingGigs);
