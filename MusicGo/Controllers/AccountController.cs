d2e761fcfea426bfc90692e7e976f0a97d11cb9d
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-06 09:36:32 +0530

    Identity Models Extended

--- MusicGo/Controllers/AccountController.cs
+++ MusicGo/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MusicGo.Models;
+using MusicGo.ViewModels;
 
 namespace MusicGo.Controllers
 {
@@ -151,7 +152,11 @@ namespace MusicGo.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { 
+                    UserName = model.Email, 
+                    Email = model.Email,
+                    Name = model.Name
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
