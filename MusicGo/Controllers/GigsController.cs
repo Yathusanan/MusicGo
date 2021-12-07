f6c96229f8bff6763a57d30fe704c1e54eff3f2b
Yathusanan Yokarajah <imyathusanan@gmail.com>
2021-12-07 14:41:31 +0530

    Edit Action Done

--- MusicGo/Controllers/GigsController.cs
+++ MusicGo/Controllers/GigsController.cs
@@ -57,10 +57,11 @@ namespace MusicGo.Controllers
         {
             var viewModel = new GigFormViewModel
             {
-                Genres = _context.Genres.ToList()
+                Genres = _context.Genres.ToList(),
+                Heading = "Add a Gig"
             };
 
-            return View(viewModel);
+            return View("GigForm", viewModel);
         }
 
         [Authorize]
@@ -70,8 +71,9 @@ namespace MusicGo.Controllers
         {
             if(!ModelState.IsValid)
             {
+                viewModel.Heading = "Add a Gig";
                 viewModel.Genres = _context.Genres.ToList();
-                return View("Create", viewModel);
+                return View("GigForm", viewModel);
             }
 
             var gig = new Gig
@@ -86,6 +88,49 @@ namespace MusicGo.Controllers
             _context.SaveChanges();
 
             return RedirectToAction("Mine", "Gigs");
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Heading = "Edit a Gig";
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            var viewModel = new GigFormViewModel
+            {
+                Heading = "Edit Gig",
+                Id = gig.Id,
+                Genres = _context.Genres.ToList(),
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Venue = gig.Venue
+            };
+
+            return View("GigForm", viewModel);
         }
     }
 }
--- MusicGo/ViewModels/GigFormViewModel.cs
+++ MusicGo/ViewModels/GigFormViewModel.cs
@@ -1,14 +1,18 @@
-﻿using MusicGo.Models;
+﻿using MusicGo.Controllers;
+using MusicGo.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MusicGo.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
         [Required]
         public string Venue { get; set; }
 
@@ -23,6 +27,21 @@ namespace MusicGo.ViewModels
         [Required]
         public int Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action 
+        { 
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update = (c => c.Update(this));
+                Expression<Func<GigsController, ActionResult>> create = (c => c.Create(this));
+
+                var action = (Id != 0) ? update : create;
+
+                return (action.Body as MethodCallExpression).Method.Name;
+            }
+        }
         public DateTime GetDateTime()
         {
             return DateTime.Parse(string.Format("{0} {1}", Date, Time));
